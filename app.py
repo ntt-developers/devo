defefc0565e7c291f142ee85445b418811612d64
ymgt <26832387+ymgt28@users.noreply.github.com>
2023-03-09 22:52:03 +0900

    change method of welcome message

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 # log level
-# TODO: setting by config value
-logging.basicConfig(level=logging.INFO)
+if os.environ.get("LOG_LEVEL") == "INFO":
+    logging.basicConfig(level=logging.INFO)
+elif os.environ.get("LOG_LEVEL") == "DEBUG":
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 # ---common function---
 
@@ -44,6 +48,32 @@ def select_random_url():
 # ---event function---
 
 @app.event("member_joined_channel")
+def check_inviter(body, logger, say):
+    logger.debug(body)
+
+    general_channel_id = os.environ.get("SLACK_GENERAL_CHANNEL_ID")
+
+    # join to general channel -> new person join
+    if body['event']['channel'] == general_channel_id:
+        guestUser = body['event']['user']
+
+        # in some cases, 'inviter' is null
+        if 'inviter' in body['event']:
+            inviteUser = body['event']['inviter']
+        else:
+            inviteUser = None
+
+        dsn = os.environ.get("PSQL_DSN_DEVO")
+        sql = "INSERT INTO inviter (guest_user_id,inviter_user_id) VALUES (%s,%s)"
+
+        with psycopg2.connect(dsn) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql,(guestUser,inviteUser))
+
+@app.event({
+    "type": "message",
+    "subtype": "channel_join"
+})
 def ask_for_introduction(body, logger, say):
     logger.debug(body)
 
@@ -51,17 +81,32 @@ def ask_for_introduction(body, logger, say):
 
     # join to general channel -> new person join
     if body['event']['channel'] == general_channel_id:
-        joinUserName = body['event']['user']
+        joinUser = body['event']['user']
 
-        # in some cases, 'inviter' is null
-        if 'inviter' in body['event']:
-            inviteUserName = body['event']['inviter']
+        # search inviter user in DB
+        dsn = os.environ.get("PSQL_DSN_DEVO")
+        sql = "SELECT seq,guest_user_id,inviter_user_id,created_at FROM inviter WHERE guest_user_id = %s AND delete_flag IS FALSE ORDER BY created_at DESC"
+
+        with psycopg2.connect(dsn) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql,(joinUser,))
+                res = cur.fetchall()
+
+        if len(res) == 0:
+            inviteUser = "unknown"
         else:
-            inviteUserName = "unknown"
-        
+            inviteUser = res[0][2]
+
         welcome_channel_id = os.environ.get("SLACK_WELCOME_CHANNEL_ID")
-        message = create_intro_message(joinUserName,inviteUserName)
+        message = create_intro_message(joinUser,inviteUser)
         say(text=message, channel=welcome_channel_id)
+
+        # delete flag
+        if len(res) != 0:
+            upd_sql = "UPDATE inviter SET delete_flag = TRUE WHERE guest_user_id = %s"
+            with psycopg2.connect(dsn) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(upd_sql,(joinUser,))
 
 # TODO:add mention command
 @app.message(re.compile('^devo '))
